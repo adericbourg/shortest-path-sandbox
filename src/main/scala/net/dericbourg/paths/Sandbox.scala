679f9ea2e1c3c32ef8393886820004b6db2fb166
Alban Dericbourg <alban@dericbourg.net>
2016-12-01 17:50:24 +0100

    Find eccentricity for a set of sources
    Signed-off-by: Alban Dericbourg <alban@dericbourg.net>

--- src/main/scala/net/dericbourg/paths/Sandbox.scala
+++ src/main/scala/net/dericbourg/paths/Sandbox.scala
@@ -59,8 +59,42 @@ object Sandbox extends App {
   }
 
   val floydWarshall = pathSourceSelector.applyingFloydWarshall(new Monoid)
+
   val shortestPathAE = floydWarshall.findShortestPath(a, e)
   shortestPathAE.getEdges.forEach(println)
+
+  // Find eccentricity of some points
+
+  def findEccentricity(sources: Seq[Vertex]): Unit = {
+    println("------------------")
+    println(s"  Eccentricity for ${sources.mkString(", ")}")
+    println("------------------")
+    val stats: Seq[Stats] = graph.vertices
+      .map { vertex =>
+        val weightsPerTarget = sources
+          .map { sourceVertex =>
+            if (sourceVertex == vertex) 0
+            else floydWarshall.findShortestPath(sourceVertex, vertex).getWeight
+          }
+        (vertex, weightsPerTarget)
+      }
+      .map { case (vertex, weights) => Stats(vertex, weights) }
+      .sortBy(_.standardDeviation)
+
+    println("Sorted solutions:")
+    stats.foreach(println)
+
+    println(s"Optimal solution for ${sources.mkString(", ")}")
+    val optimalTarget = stats.head
+    println(optimalTarget)
+    println()
+  }
+
+  findEccentricity(Seq(a, g))
+  findEccentricity(Seq(f, h, i))
+  findEccentricity(Seq(a, b, e))
+  findEccentricity(Seq(f, g, h))
+  findEccentricity(Seq(a, b)) // This corner-case sucks: we should choose a or b
 }
 
 case class Vertex(name: String)
@@ -70,11 +104,44 @@ class WeightedEdge(val weight: Int, val head: Vertex, val tail: Vertex) {
   override def toString = s"WeightedEdge(weight=$weight, head=$head, tail=$tail)"
 }
 
+class Stats(vertex: Vertex, values: Seq[Int]) {
+
+  import Stats._
+
+  val standardDeviation: Double = stdDev(values)
+  val sum: Int = values.sum
+
+
+  override def toString = s"Stats(${vertex.name}: (stdDev: $standardDeviation, sum: $sum))"
+}
+
+object Stats {
+
+  import Numeric.Implicits._
+
+  def apply(vertex: Vertex, values: Seq[Int]): Stats = new Stats(vertex, values)
+
+  private[Stats] def mean[T: Numeric](xs: Iterable[T]): Double = xs.sum.toDouble / xs.size
+
+  private[Stats] def variance[T: Numeric](xs: Iterable[T]): Double = {
+    val avg = mean(xs)
+    xs.map(_.toDouble).map(a => math.pow(a - avg, 2)).sum / xs.size
+  }
+
+  private[Stats] def stdDev[T: Numeric](xs: Iterable[T]): Double = {
+    math.sqrt(variance(xs))
+  }
+}
+
 class SandboxGraph {
+
+  import scala.collection.JavaConverters._
 
   val underlying = new UndirectedMutableGraph[Vertex, WeightedEdge]()
 
   def addVertex(vertex: Vertex): Unit = underlying.addVertex(vertex)
+
+  def vertices: Seq[Vertex] = underlying.getVertices.asScala.toSeq
 
   def addEdge(edge: WeightedEdge): Unit = underlying.addEdge(edge.head, edge, edge.tail)
 }
