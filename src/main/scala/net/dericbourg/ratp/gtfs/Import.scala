b3822799571fbe9d4b256b6bd22a8d0d464a2b65
Alban Dericbourg <alban@dericbourg.net>
2016-12-09 13:54:53 +0100

    Mare GTFS import more resilient to errors
    Signed-off-by: Alban Dericbourg <alban@dericbourg.net>

--- src/main/scala/net/dericbourg/ratp/gtfs/Import.scala
+++ src/main/scala/net/dericbourg/ratp/gtfs/Import.scala
@@ -18,90 +18,113 @@ object Import extends App {
 
   val connection = datasource.getConnection
 
-  val gtfsRootDirectory = new File(s"${System.getProperty("user.home")}/tmp/lines")
+  val gtfsRootDirectory = new File(s"${System.getProperty("user.home")}/tmp/RATP_GTFS")
   val lineDirectories: Array[File] = gtfsRootDirectory.
     listFiles(new FilenameFilter() {
       override def accept(dir: File, name: String): Boolean = name.startsWith("RATP_GTFS_")
     }).
     filter(_.isDirectory)
 
+    lineDirectories.foreach { directory =>
+      println(s"Parsing routes from ${directory.getName}")
+      CSVReader.open(new File(directory, "routes.txt"))
+        .allWithHeaders()
+        .map(Route.parse)
+        .grouped(1000)
+        .toList
+        .par
+        .foreach { routeBatch =>
+          val preparedStatement = connection.prepareStatement(Query.Route)
+          routeBatch.foreach { (route: Route) =>
+            import route._
+            preparedStatement.setLong(1, routeId)
+            preparedStatement.setString(2, routeShortName)
+            preparedStatement.setString(3, routeLongName)
+            preparedStatement.setString(4, routeDesc)
+            preparedStatement.addBatch()
+          }
+          preparedStatement.executeBatch()
+        }
+    }
+
+    lineDirectories.foreach { directory =>
+      println(s"Parsing stops from ${directory.getName}")
+      CSVReader.open(new File(directory, "stops.txt"))
+        .allWithHeaders()
+        .map(Stop.parse)
+        .grouped(1000)
+        .toList
+        .par
+        .foreach { stopBatch =>
+          val preparedStatement = connection.prepareStatement(Query.Stop)
+          stopBatch.foreach { (stop: Stop) =>
+            import stop._
+            preparedStatement.setLong(1, stopId)
+            preparedStatement.setString(2, stopName)
+            preparedStatement.setString(3, stopDesc)
+            preparedStatement.setDouble(4, stopLat)
+            preparedStatement.setDouble(5, stopLon)
+            preparedStatement.setInt(6, locationType)
+            preparedStatement.setString(7, parentStation)
+            preparedStatement.addBatch()
+          }
+          preparedStatement.executeBatch()
+        }
+    }
+
+    lineDirectories.foreach { directory =>
+      println(s"Parsing trips from ${directory.getName}")
+      CSVReader.open(new File(directory, "trips.txt"))
+        .allWithHeaders()
+        .map(Trip.parse)
+        .grouped(1000)
+        .toList
+        .par
+        .foreach { tripBatch =>
+          val preparedStatement = connection.prepareStatement(Query.Trip)
+          tripBatch.foreach { (trip: Trip) =>
+            import trip._
+            preparedStatement.setLong(1, tripId)
+            preparedStatement.setLong(2, routeId)
+            preparedStatement.setLong(3, serviceId)
+            preparedStatement.setString(4, tripShortName)
+            preparedStatement.setString(5, tripShortName)
+            preparedStatement.addBatch()
+          }
+          preparedStatement.executeBatch()
+        }
+    }
+
+    lineDirectories.foreach { directory =>
+      println(s"Parsing stop times from ${directory.getName}")
+      CSVReader.open(new File(directory, "stop_times.txt"))
+        .allWithHeaders()
+        .map(StopTime.parse)
+        .grouped(1000)
+        .toList
+        .par
+        .foreach { stopTimesBatch =>
+          val preparedStatement = connection.prepareStatement(Query.StopTime)
+          stopTimesBatch.foreach { (stopTime: StopTime) =>
+            import stopTime._
+            preparedStatement.setLong(1, tripId)
+            preparedStatement.setLong(2, arrivalTime.toMillis)
+            preparedStatement.setLong(3, departureTime.toMillis)
+            preparedStatement.setLong(4, stopId)
+            preparedStatement.addBatch()
+          }
+          preparedStatement.executeBatch()
+        }
+    }
+
   lineDirectories.foreach { directory =>
-    CSVReader.open(new File(directory, "routes.txt"))
-      .allWithHeaders()
-      .map(Route.parse)
-      .grouped(100)
-      .foreach { routeBatch =>
-        val preparedStatement = connection.prepareStatement(Query.Route)
-        routeBatch.foreach { (route: Route) =>
-          import route._
-          preparedStatement.setLong(1, routeId)
-          preparedStatement.setString(2, routeShortName)
-          preparedStatement.setString(3, routeLongName)
-          preparedStatement.setString(4, routeDesc)
-          preparedStatement.addBatch()
-        }
-        preparedStatement.executeBatch()
-      }
-
-    CSVReader.open(new File(directory, "stops.txt"))
-      .allWithHeaders()
-      .map(Stop.parse)
-      .grouped(100)
-      .foreach { stopBatch =>
-        val preparedStatement = connection.prepareStatement(Query.Stop)
-        stopBatch.foreach { (stop: Stop) =>
-          import stop._
-          preparedStatement.setLong(1, stopId)
-          preparedStatement.setString(2, stopName)
-          preparedStatement.setString(3, stopDesc)
-          preparedStatement.setDouble(4, stopLat)
-          preparedStatement.setDouble(5, stopLon)
-          preparedStatement.setInt(6, locationType)
-          preparedStatement.setString(7, parentStation)
-          preparedStatement.addBatch()
-        }
-        preparedStatement.executeBatch()
-      }
-
-    CSVReader.open(new File(directory, "trips.txt"))
-      .allWithHeaders()
-      .map(Trip.parse)
-      .grouped(100)
-      .foreach { tripBatch =>
-        val preparedStatement = connection.prepareStatement(Query.Trip)
-        tripBatch.foreach { (trip: Trip) =>
-          import trip._
-          preparedStatement.setLong(1, tripId)
-          preparedStatement.setLong(2, routeId)
-          preparedStatement.setLong(3, serviceId)
-          preparedStatement.setString(4, tripShortName)
-          preparedStatement.setString(5, tripShortName)
-          preparedStatement.addBatch()
-        }
-        preparedStatement.executeBatch()
-      }
-
-    CSVReader.open(new File(directory, "stop_times.txt"))
-      .allWithHeaders()
-      .map(StopTime.parse)
-      .grouped(100)
-      .foreach { stopTimesBatch =>
-        val preparedStatement = connection.prepareStatement(Query.StopTime)
-        stopTimesBatch.foreach { (stopTime: StopTime) =>
-          import stopTime._
-          preparedStatement.setLong(1, tripId)
-          preparedStatement.setLong(2, arrivalTime.toMillis)
-          preparedStatement.setLong(3, departureTime.toMillis)
-          preparedStatement.setLong(4, stopId)
-          preparedStatement.addBatch()
-        }
-        preparedStatement.executeBatch()
-      }
-
+    println(s"Parsing transfers from ${directory.getName}")
     CSVReader.open(new File(directory, "transfers.txt"))
       .allWithHeaders()
       .map(Transfer.parse)
-      .grouped(100)
+      .grouped(1000)
+      .toList
+      .par
       .foreach { transfersBatch =>
         val preparedStatement = connection.prepareStatement(Query.Transfer)
         transfersBatch.foreach { (transfer: Transfer) =>
@@ -110,6 +133,8 @@ object Import extends App {
           preparedStatement.setLong(2, toStopId)
           preparedStatement.setString(3, transferType)
           preparedStatement.setLong(4, minTransferTime)
+          preparedStatement.setLong(5, fromStopId)
+          preparedStatement.setLong(6, toStopId)
           preparedStatement.addBatch()
         }
         preparedStatement.executeBatch()
@@ -147,6 +172,8 @@ object Query {
   val Transfer: String =
     """
       |insert into transfer (from_stop_id, to_stop_id, transfer_type, min_transfer_time)
-      |values (?, ?, ?, ?)
+      |select ?, ?, ?, ?
+      |where exists (select 1 from stop where id = ?)
+      |and exists (select 1 from stop where id = ?);
     """.stripMargin
 }
