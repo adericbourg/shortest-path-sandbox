4f9cbe93f9a355bcf82c4d44efa1f2b4af95c62c
Alban Dericbourg <alban@dericbourg.net>
2016-12-12 16:27:45 +0100

    Approximate links and weights between stations
    Signed-off-by: Alban Dericbourg <alban@dericbourg.net>

---
+++ src/main/resources/net/dericbourg/ratp/gtfs/all_links.sql
@@ -0,0 +1,33 @@
+CREATE OR REPLACE VIEW all_links (
+    start_stop_id,
+    start_stop_name,
+    arrival_stop_id,
+    arrival_stop_name,
+    connection_duration
+)
+AS (
+  SELECT
+    start_stop_id,
+    start_stop_name,
+    arrival_stop_id,
+    arrival_stop_name,
+    avg(connection_duration) AS connection_duration
+  FROM (
+         SELECT
+           start_stop_id,
+           start_stop_name,
+           arrival_stop_id,
+           arrival_stop_name,
+           connection_duration
+         FROM enriched_connection
+         UNION
+         SELECT
+           start_stop_id,
+           start_stop_name,
+           arrival_stop_id,
+           arrival_stop_name,
+           connection_duration
+         FROM enriched_transfer
+       ) u
+  GROUP BY start_stop_id, arrival_stop_id, start_stop_name, arrival_stop_name
+);
