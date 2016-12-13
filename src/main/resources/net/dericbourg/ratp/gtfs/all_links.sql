cdebd49434168326694c060893272a4fe6bdc5f4
Alban Dericbourg <alban@dericbourg.net>
2016-12-13 22:05:36 +0100

    Add source on `all_links` view
    Signed-off-by: Alban Dericbourg <alban@dericbourg.net>

--- src/main/resources/net/dericbourg/ratp/gtfs/all_links.sql
+++ src/main/resources/net/dericbourg/ratp/gtfs/all_links.sql
@@ -3,7 +3,8 @@ CREATE OR REPLACE VIEW all_links (
     start_stop_name,
     arrival_stop_id,
     arrival_stop_name,
-    connection_duration
+    connection_duration,
+    source
 )
 AS (
   SELECT
@@ -11,14 +12,16 @@ AS (
     start_stop_name,
     arrival_stop_id,
     arrival_stop_name,
-    avg(connection_duration) AS connection_duration
+    avg(connection_duration) AS connection_duration,
+    source
   FROM (
          SELECT
            start_stop_id,
            start_stop_name,
            arrival_stop_id,
            arrival_stop_name,
-           connection_duration
+           connection_duration,
+           'connection' AS source
          FROM enriched_connection
          UNION
          SELECT
@@ -26,7 +29,8 @@ AS (
            start_stop_name,
            arrival_stop_id,
            arrival_stop_name,
-           connection_duration
+           connection_duration,
+           'transfer' AS source
          FROM enriched_transfer
          UNION
          SELECT
@@ -34,8 +38,9 @@ AS (
            start_stop_name,
            arrival_stop_id,
            arrival_stop_name,
-           connection_duration
+           connection_duration,
+           'self' AS source
          FROM station_reverse
        ) u
-  GROUP BY start_stop_id, arrival_stop_id, start_stop_name, arrival_stop_name
+  GROUP BY start_stop_id, arrival_stop_id, start_stop_name, arrival_stop_name, source
 );
