52e56b79b44a1da55e23ef01f80602f6e4c8eca9
Alban Dericbourg <alban@dericbourg.net>
2016-12-14 13:34:39 +0100

    Fix UsingPostgres
    Bad try hid exceptions
    
    Signed-off-by: Alban Dericbourg <alban@dericbourg.net>

--- src/main/scala/net/dericbourg/db/UsingPostgres.scala
+++ src/main/scala/net/dericbourg/db/UsingPostgres.scala
@@ -4,10 +4,8 @@ import java.sql.Connection
 
 import org.postgresql.ds.PGPoolingDataSource
 
-import scala.util.Try
-
 object UsingPostgres {
-  lazy val datasource = {
+  private[db] lazy val datasource = {
     val p = new PGPoolingDataSource()
     p.setDatabaseName("postgres")
     p.setUser("postgres")
@@ -16,9 +14,13 @@ object UsingPostgres {
     p
   }
 
-  def apply[R](f: Connection => R): Try[R] = {
-    tryWith(datasource.getConnection) { connection =>
-      f(connection)
+  def apply[R](body: Connection => R): R = {
+    val connection = datasource.getConnection
+    try {
+      body(connection)
+    }
+    finally {
+      connection.close()
     }
   }
 }
